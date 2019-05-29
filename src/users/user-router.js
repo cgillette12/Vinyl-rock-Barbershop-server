4c131eea6eeae24efbb5cbfb751d6221950ea673
Cody Gillette <cgillette1312@gmail.com>
2019-05-28 17:32:09 -0700

    user-router

--- src/users/user-router.js
+++ src/users/user-router.js
@@ -14,4 +14,43 @@ usersRouter
         return res.status(400).json({
           error: `Missing '${field}' in request body`
         });
+    const passwordError = UsersService.validatePassword(password);
+
+    if (passwordError)
+      return res.status(400).json({ error: passwordError });
+    UsersService.hasUserWithUserName(
+      req.app.get('db'),
+      user_name
+    )
+      .then(hasUserWithUserName => {
+        if(hasUserWithUserName){
+          return res.status(400).json({error:'Username already exsits'});
+        }
+        return UsersService.hashPassword(password)
+          .then(hashedPassword => {
+
+            const newUser = {
+              first_name, 
+              last_name,
+              user_name, 
+              email,
+              phone_number,
+              date_created: 'now()'
+            };
+
+            return UsersService.insetUser(
+              req.app.get('db'),
+              newUser
+            )
+              .then(user => {
+                res
+                  .status(201)
+                  .location(path.posix.join(req.originalUrl, `/${user.id}`))
+                  .json(UsersService.serializeUser(user));
+              });
+          });   
+      })
+      .catch(next);
   });
+
+module.exports = usersRouter;
